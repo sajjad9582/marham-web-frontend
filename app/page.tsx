fe45d29d34059f883b13cd144795357fd3eda161
Sajjad <sajjad.hussain@marham.com>
2026-06-04 10:11:14 +0500

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-svh  w-full items-center justify-center bg-mainblue p-6">
+     
+     <h1 className="text-2xl font-bold bg-deepblue text-white p-4 rounded-md">Hello World</h1>
+    </div>
+  )
+}
